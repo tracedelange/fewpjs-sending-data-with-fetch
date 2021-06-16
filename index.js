d079f1a217b6b84b7504927594f4d7ae6425ce38
Trace <tracedelange@me.com>
2021-06-16 09:19:10 -0500

    commit tree e457f30527712233fd119d66eb2167a9b15c00c3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+
+
+function submitData(username, email) {
+    const formData = {
+        name: username,
+        email: email
+    }
+
+    const postConfigObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', postConfigObject)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data) {
+            const body = document.getElementsByTagName('body')[0]
+            let id = document.createElement('p')
+            id.innerHTML = data.id
+            body.appendChild(id)
+        })
+        .catch(function(error){
+            console.log(error.message)
+            const body = document.getElementsByTagName('body')[0]
+            let errorMessage = document.createElement('p')
+            errorMessage.innerHTML = error.message
+            body.appendChild(errorMessage)
+        })
+}
